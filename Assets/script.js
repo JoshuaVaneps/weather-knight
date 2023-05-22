a1c9a7beeee3a242fa9cf32711388fff3f5b18f1
Joshua Vaneps <joshuavaneps@yahoo.com>
2023-05-22 09:07:16 -0400

    updated the styling of the site and added 3d icons

--- Assets/script.js
+++ Assets/script.js
@@ -31,6 +31,7 @@ $(function () {
         inputLat = data[0].lat;
         inputLon = data[0].lon;
         // fetching all relevant data from api usihg the lon and lat from geocode api
+
         fetch(
           `https://api.openweathermap.org/data/2.5/forecast?lat=${inputLat}&lon=${inputLon}&appid=${apiKey}&units=imperial`
         )
@@ -39,6 +40,7 @@ $(function () {
             currentTemp = `${data.list[0].main.temp}`;
             console.log("current Temp= " + currentTemp);
             currentHumidity = `${data.list[0].main.humidity}`;
+            currentPressure = `${data.list[0].main.pressure}`;
             currentFeels = `${data.list[0].main.feels_like}`;
             currentMin = `${data.list[0].main.temp_min}`;
             currentMax = `${data.list[0].main.temp_max}`;
@@ -50,6 +52,24 @@ $(function () {
             console.log("max and min = " + currentMax + " & " + currentMin);
             console.log("feels like " + currentFeels);
             console.log(currentIcon);
+            // current advanced temp info
+            $("#max-temp").children("h2").html("");
+
+            $("#max-temp")
+              .children("h2")
+              .append(currentMax + "\xB0" + "F");
+
+            $("#pressure").children("h2").html("");
+
+            $("#pressure")
+              .children("h2")
+              .append(currentPressure + " hPa");
+
+            $("#feels-like").children("h2").html("");
+
+            $("#feels-like")
+              .children("h2")
+              .append(currentFeels + "\xB0" + "F");
 
             // 5 day forecast inputs
             for (let i = 0; i < 6; i++) {
@@ -88,6 +108,48 @@ $(function () {
                 "Wind Speed: " + forecastWind + "mph"
               );
             }
+          });
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${inputLat}&lon=${inputLon}&appid=${apiKey}&units=imperial`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            var sunriseDate = `${data.sys.sunrise}` * 1000;
+            var d = new Date(sunriseDate);
+            let sunriseHours = d.getHours();
+            let sunriseMinutes = "0" + d.getMinutes();
+            var AmOrPm = sunriseHours >= 12 ? "pm" : "am";
+            sunriseHours = sunriseHours % 12 || 12;
+            let sunRise = sunriseHours + ":" + sunriseMinutes.substr(-2);
+
+            console.log("sunrises at " + sunRise + AmOrPm);
+            $("#sunrise").children("h2").html("");
+
+            $("#sunrise")
+              .children("h2")
+              .append(sunRise + AmOrPm);
+
+            var sunsetDate = `${data.sys.sunset}` * 1000;
+            var d = new Date(sunsetDate);
+            let sunsetHours = d.getHours();
+            let sunsetMinutes = "0" + d.getMinutes();
+            var AmOrPm = sunsetHours >= 12 ? "pm" : "am";
+            sunsetHours = sunsetHours % 12 || 12;
+            let sunSet = sunsetHours + ":" + sunsetMinutes.substr(-2);
+
+            console.log("sunsets at " + sunSet + AmOrPm);
+            $("#sunset").children("h2").html("");
+
+            $("#sunset")
+              .children("h2")
+              .append(sunSet + AmOrPm);
+
+            var cloudiness = `${data.clouds.all}`;
+            $("#cloud").children("h2").html("");
+
+            $("#cloud")
+              .children("h2")
+              .append(cloudiness + "%");
           });
       });
   });
